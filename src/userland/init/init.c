488b33d2ba0e0c59f25bfdba44e84f2a08469b2c
biosfood <leisenhau@gmail.com>
2025-03-29 18:29:47 +0100

    clean up bin/init

--- src/userland/init/init.c
+++ src/userland/init/init.c
@@ -19,29 +19,22 @@ void parallelOut(uint32_t data, uint32_t dataLength) {
     }
 }
 
-void writeBulk(char *buffer) {
-    while (*buffer) {
-        parallelOut(*buffer, 0);
-        buffer++;
-    }
-}
+char buffer[1024];
 
 int test() {
-    int fd = open("/dev/1", 0);
-    char buffer = 'X';
+    int fd = open("/dev/serout", 0);
     while (1) {
-        const uint32_t count = read(fd, &buffer, 1);
-        if (count == 1) {
-            parallelOut(buffer, 0);
+        const int32_t count = read(fd, buffer, 1024);
+        for (int i = 0; i < count; ++i) {
+            parallelOut(buffer[i], 0);
         }
     }
 }
 
 int main() {
-    mkfifo("/dev/1");
-    const int fd = open("/dev/1", 0);
-    pthread_create(0, 0, (void*) test, 0);
-    writeBulk("Hello World!\n");
-    char *message = "This is a test\n";
+    mkfifo("/dev/serout");
+    const int fd = open("/dev/serout", 0);
+    pthread_create(0, 0, (void *)test, 0);
+    char *message = "Hello World\n";
     write(fd, message, 15);
 }

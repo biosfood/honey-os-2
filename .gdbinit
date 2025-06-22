e59937531932e77f9d5e11348c533bd9bde5f650
biosfood <leisenhau@gmail.com>
2025-06-22 16:43:01 +0200

    add list printing for debugging purposes

--- .gdbinit
+++ .gdbinit
@@ -1,2 +1,8 @@
 set arch i386
 set max-value-size unlimited
+
+python
+import sys
+sys.path.insert(0, '.')
+from list_print import *
+end
---
+++ list_print.py
@@ -0,0 +1,53 @@
+import gdb
+
+# Define your type map: {StructName: {fieldName: pointeeType}}
+type_map = {
+    "struct Process": {
+        "virtual_memory_entries": "VirtualMemoryEntry",
+    },
+    "struct VirtualMemoryEntry": {
+        "mappings": "MemoryMapping",
+    },
+}
+
+class CustomStructPrinter(gdb.ValuePrinter):
+    def __init__(self, val, map):
+        self.val = val
+        self.type = val.type.strip_typedefs()
+        self.map = map
+
+    def to_string(self):
+        return f"{self.type.tag or self.type.name}"
+
+    def children(self):
+        fields = self.type.fields()
+        for field in fields:
+            if field.name in self.map:
+                elements = []
+                current = self.val[field]
+                type = gdb.lookup_type(self.map[field.name] + " *")
+                while current:
+                    elements.append(current['data'])
+                    current = current['next']
+                expr_items = ', '.join(f'(void*){v}' for v in elements)
+
+                expr = f'({type}[{len(elements)}]){{ {expr_items} }}'
+                value = gdb.parse_and_eval(expr)
+                yield field.name, value
+                # yield field.name + ' - raw', self.val[field]
+            else:
+                yield field.name, self.val[field]
+    def display_hint(self):
+        return "struct"
+
+
+# Dispatcher
+def printer_lookup(val):
+    typename = str(val.type.strip_typedefs())
+    if typename in type_map:
+        return CustomStructPrinter(val, type_map[typename])
+    return None
+
+
+# Register globally
+gdb.pretty_printers.append(printer_lookup)

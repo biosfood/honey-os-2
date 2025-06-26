a874158b3463ff637495dff62b2bfe11449543e1
biosfood <leisenhau@gmail.com>
2025-06-26 20:34:48 +0200

    /bin/serial: actual interface the SERIAL port

--- src/userland/serial/main.c
+++ src/userland/serial/main.c
@@ -1,64 +1,46 @@
-#include <fnctl.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <dirent.h>
 #include <fnctl.h>
 #include <pthread.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-
-int data_fd, status_fd, control_fd; // file descriptors
-
-#define ioOut(port, data, len)                                                 \
-    {                                                                          \
-        buf = data;                                                            \
-        pwrite(portFd, &buf, len, port);                                       \
-    }
-#define ioIn(port, len)                                                        \
-    ({                                                                         \
-        pread(portFd, &buf, len, port);                                        \
-        buf;                                                                   \
-    })
-#define U32(x) ((uint32_t)(uintptr_t)(x))
-
-void parallelOut(const uint32_t data) {
-    if (data == '\n') {
-        parallelOut('\r');
-    }
-    uint32_t status;
-    do {
-        read(status_fd, &status, 1);
-    } while (!(status & 0x80));
-
-    write(data_fd, (void*)&data, 1);
-
-    uint8_t control;
-    read(control_fd, &control, 1);
-    control |= 1;
-    write(control_fd, &control, 1);
-    control &= ~1;
-    write(control_fd, &control, 1);
-
-    do {
-        read(status_fd, &status, 1);
-    } while (!(status & 0x80));
-}
+#include <unistd.h>
 
 char buffer[1024];
 
 void main() {
-    data_fd = open("/dev/port/888", 0);
-    status_fd = open("/dev/port/889", 0);
-    control_fd = open("/dev/port/890", 0);
     int fd = open("/dev/serout", 0);
+
+    int rbr = open("/dev/port/1016", 0);
+    int ier = open("/dev/port/1017", 0);
+    int irr = open("/dev/port/1018", 0);
+    int lcr = open("/dev/port/1019", 0);
+    int mcr = open("/dev/port/1020", 0);
+    int lsr = open("/dev/port/1021", 0);
+    int msr = open("/dev/port/1022", 0);
+    int scr = open("/dev/port/1023", 0);
+
+    uint8_t data = 1;
+    write(ier, &data, 1);
+    data = 0x80;
+    write(lcr, &data, 1);
+    data = 3;
+    write(rbr, &data, 1);
+    data = 0;
+    write(ier, &data, 1);
+    data = 3;
+    write(lcr, &data, 1);
+    data = 0xC7;
+    write(irr, &data, 1);
+    data = 0x0B;
+    write(mcr, &data, 1);
+
+
     while (1) {
         const int32_t count = read(fd, buffer, 1024);
         for (int i = 0; i < count; ++i) {
-            parallelOut(buffer[i]);
+            write(rbr, buffer + i, 1);
         }
     }
 }

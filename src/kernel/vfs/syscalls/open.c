5d28b02e1ffb24584091ea00e81fdd3b0e430344
biosfood <leisenhau@gmail.com>
2025-09-05 21:21:30 +0200

    rework open() syscall for multi-turn opening

--- src/kernel/vfs/syscalls/open.c
+++ src/kernel/vfs/syscalls/open.c
@@ -1,50 +1,101 @@
 //
 // Created by lukas on 9/5/25.
 //
+#include "fnctl.h"
 #include <process.h>
 #include <stddef.h>
 #include <vfs.h>
-#include "fnctl.h"
+
+enum OpenStage {
+    OPEN_PRE = 0,
+    OPEN_MAIN,
+    OPEN_CREATE_FIND,
+    OPEN_CREATE_POST,
+    OPEN_POST,
+};
+
+struct OpenState {
+    enum OpenStage stage;
+    char *filename;
+    char *directory_file_name;
+    void *scratchpad; // for FileSystem->getFile()
+    File *file;
+    File *directory;
+    uint32_t lastSlashPosition;
+};
 
 void handleOpenSyscall(ProcessThread *thread) {
-    char *filename =
-        copy_string_from_process(thread->process, PTR(thread->parameters[0]));
-    File *file;
-    void *scratchpad = NULL;
-    thread->process->container->vfs->type->getFile(
-        thread->process->container->vfs, filename, thread, &file, &scratchpad);
-    listAdd(&threads_to_process, thread);
-    if (file == NULL) {
-        if (thread->parameters[1] & O_CREAT) {
-            char *directoryFileName = malloc(strlen(filename));
-            memcpy(filename, directoryFileName, strlen(filename) + 1);
-            int lastSlashPosition = strlen(filename) - 1;
-            while (lastSlashPosition &&
-                   directoryFileName[lastSlashPosition] != '/') {
-                lastSlashPosition--;
-                   }
-            directoryFileName[lastSlashPosition] = 0;
-            File *dir;
-            scratchpad = NULL;
+    struct OpenState *state = thread->threadProcessingState;
+    thread->run = false; // must explicitly be set.
+    switch (state->stage) {
+    case OPEN_PRE:
+        state->filename = copy_string_from_process(thread->process,
+                                                   PTR(thread->parameters[0]));
+        state->scratchpad = NULL;
+        state->file = NULL;
+        state->stage = OPEN_MAIN;
+        // fallthrough
+    case OPEN_MAIN:
+        FileOperationStatus status =
             thread->process->container->vfs->type->getFile(
-                thread->process->container->vfs, directoryFileName, thread, &dir, &scratchpad);
-            if (dir->type != FILE_TYPE_DIRECTORY) {
-                thread->returnValue = -1;
-                return;
-            }
-            file = dir->file_system->type->create(
-                dir, filename + lastSlashPosition + 1, FILE_TYPE_FILE);
-        } else {
-            thread->returnValue = -1;
-            return;
+                thread->process->container->vfs, state->filename, thread,
+                &state->file, &state->scratchpad);
+        if (status != FILE_OPERATION_DONE) {
+            break;
         }
+        if (state->file || !(thread->parameters[1] & O_CREAT)) {
+            // found it
+            state->stage = OPEN_POST;
+            goto post;
+        }
+        // file not found and O_CREAT is set, need to create it now...
+        state->stage = OPEN_CREATE_FIND;
+        state->directory_file_name = malloc(strlen(state->filename));
+        memcpy(state->filename, state->directory_file_name,
+               strlen(state->filename) + 1);
+        state->lastSlashPosition = strlen(state->filename) - 1;
+        while (state->lastSlashPosition &&
+               state->directory_file_name[state->lastSlashPosition] != '/') {
+            state->lastSlashPosition--;
+        }
+        state->directory_file_name[state->lastSlashPosition] = 0;
+        // fallthrough
+    case OPEN_CREATE_FIND:
+        status = thread->process->container->vfs->type->getFile(
+            thread->process->container->vfs, state->directory_file_name, thread,
+            &state->directory, &state->scratchpad);
+        if (status != FILE_OPERATION_DONE) {
+            break;
+        }
+        if (state->directory == NULL ||
+            state->directory->type != FILE_TYPE_DIRECTORY) {
+            state->stage = OPEN_POST;
+            goto post;
+        }
+        state->stage = OPEN_CREATE_POST;
+        // fallthrough
+    case OPEN_CREATE_POST:
+        state->file = state->directory->file_system->type->create(
+            state->directory, state->filename + state->lastSlashPosition + 1,
+            FILE_TYPE_FILE);
+    case OPEN_POST:
+    post:
+        if (state->file == NULL) {
+            thread->returnValue = -1;
+        } else {
+            FileDescriptor *file_descriptor =
+                allocateFileDescriptor(thread->process);
+            file_descriptor->file = state->file;
+            file_descriptor->process = thread->process;
+            file_descriptor->offset = 0;
+            thread->returnValue = file_descriptor->id;
+            listAdd(&state->file->file_descriptors, file_descriptor);
+        }
+        free(state->filename);
+        if (state->directory_file_name) {
+            free(state->directory_file_name);
+        }
+        thread->run = true;
+        listAdd(&threads_to_process, thread);
     }
-    free(filename);
-    FileDescriptor *file_descriptor = allocateFileDescriptor(thread->process);
-    file_descriptor->file = file;
-    file_descriptor->process = thread->process;
-    file_descriptor->offset = 0;
-    thread->returnValue = file_descriptor->id;
-    listAdd(&file->file_descriptors, file_descriptor);
 }
-

6e833a74028abf79e71329069ab8d24f1ecac286
biosfood <leisenhau@gmail.com>
2026-04-03 00:02:58 +0200

    elf: fix some memory mapping issues

--- src/kernel/process/elf.c
+++ src/kernel/process/elf.c
@@ -3,13 +3,13 @@
 #include <stddef.h>
 
 // You'll need to define the AT_* values. They are standard in <elf.h>.
-#define AT_NULL   0  /* End of vector */
-#define AT_PHDR   3  /* Phdr base address */
-#define AT_PHNUM  5  /* Number of phdrs */
-#define AT_PHENT  4  /* Size of one phdr */
+#define AT_NULL 0  /* End of vector */
+#define AT_PHDR 3  /* Phdr base address */
+#define AT_PHNUM 5 /* Number of phdrs */
+#define AT_PHENT 4 /* Size of one phdr */
 
 void build_starting_stack(ProcessThread *thread, void *start_position,
-    uintptr_t phdr_vaddr, int phnum, int phentsize) {
+                          uintptr_t phdr_vaddr, int phnum, int phentsize) {
     Process *process = thread->process;
     // build the initial stack, for starters just with a single page
     PhysicalMemoryEntry *physical = get_single_page_physical_memory_entry();
@@ -17,8 +17,8 @@ void build_starting_stack(ProcessThread *thread, void *start_position,
     // extend it any time.
     VirtualMemoryEntry *virtual = malloc(sizeof(VirtualMemoryEntry));
     virtual->process = process;
-    virtual->virtual = ADDRESS(
-        findMultiplePages(&process->memory_information, 2048));
+    virtual->virtual =
+        ADDRESS(findMultiplePages(&process->memory_information, 2048));
     virtual->type = MEM_TYPE_STACK;
     virtual->size = 4096 * 2048;
     reservePagesCount(&process->memory_information, PAGE_ID(virtual->virtual),
@@ -106,38 +106,59 @@ ProcessThread *processLoadELF(Process *process, void *file_data) {
     reservePagesCount(&(process->memory_information), 0, 0x10);
     uintptr_t phdr_virtual = 0;
     for (uint32_t i = 0; i < header->programHeaderEntryCount; i++) {
-        if ((programHeader->segmentType == 6 || programHeader->dataOffset == 0) && !phdr_virtual) {
-            phdr_virtual = programHeader->virtualAddress + header->programHeaderTablePosition;
+        if (programHeader->segmentType != 1) {
+            programHeader = (void *)programHeader + header->programHeaderEntrySize;
+            continue;
+        }
+        if ((programHeader->segmentType == 6 ||
+             programHeader->dataOffset == 0) &&
+            !phdr_virtual) {
+            phdr_virtual = programHeader->virtualAddress +
+                           header->programHeaderTablePosition;
         }
         if (programHeader->segmentMemorySize == 0) {
             continue;
         }
+        uintptr_t vaddr = programHeader->virtualAddress;
+        uintptr_t memsz = programHeader->segmentMemorySize;
+        uintptr_t filesz = programHeader->segmentFileSize;
+        uintptr_t offset = programHeader->dataOffset;
+
+        uintptr_t start_page = vaddr & ~0xFFF;
+        uintptr_t end_page = (vaddr + memsz + 0xFFF) & ~0xFFF;
+        uint32_t num_pages = (end_page - start_page) >> 12;
+
         VirtualMemoryEntry *virtual = malloc(sizeof(VirtualMemoryEntry));
-        virtual->virtual = ADDRESS(PAGE_ID(PTR(programHeader->virtualAddress)));
+        virtual->virtual = ADDRESS(PAGE_ID(PTR(start_page)));
         virtual->process = process;
         virtual->type = MEM_TYPE_PROGRAM_DATA;
         virtual->mappings = NULL;
-        virtual->size = MAX(programHeader->segmentMemorySize, 0x1000);
+        virtual->size = num_pages * 0x1000; // Size spans all allocated pages
         listAdd(&process->virtual_memory_entries, virtual);
 
-        PhysicalMemoryEntry *physical_memory_entry =
-            get_single_page_physical_memory_entry();
-        void *mapped = mapTemporaryA(physical_memory_entry->physical);
-        memset(mapped, 0, 0x1000);
-        memcpy(file_data + programHeader->dataOffset,
-               mapped + (programHeader->virtualAddress & 0xFFF),
-               MIN(0x1000 - (programHeader->virtualAddress & 0xFFF), programHeader->segmentFileSize));
-        MAP(virtual, physical_memory_entry, PTR(programHeader->virtualAddress));
-        for (uint32_t page = 0x1000; page < programHeader->segmentMemorySize;
-             page += 0x1000) {
-            physical_memory_entry =
+        for (uint32_t i = 0; i < num_pages; i++) {
+            uintptr_t current_page_vaddr = start_page + (i * 0x1000);
+
+            PhysicalMemoryEntry *physical_memory_entry =
                 get_single_page_physical_memory_entry();
-            mapped = mapTemporaryA(physical_memory_entry->physical);
+            void *mapped = mapTemporaryA(physical_memory_entry->physical);
+
             memset(mapped, 0, 0x1000);
-            memcpy(file_data + programHeader->dataOffset + page, mapped,
-                   MIN(0x1000, programHeader->segmentFileSize - page));
-            MAP(virtual, physical_memory_entry,
-                PTR(programHeader->virtualAddress + page));
+
+            uintptr_t page_data_start = MAX(current_page_vaddr, vaddr);
+            uintptr_t page_data_end =
+                MIN(current_page_vaddr + 0x1000, vaddr + filesz);
+
+            if (page_data_start < page_data_end) {
+                uintptr_t copy_size = page_data_end - page_data_start;
+                uintptr_t file_offset = offset + (page_data_start - vaddr);
+                uintptr_t page_offset = page_data_start & 0xFFF;
+
+                memcpy(file_data + file_offset, mapped + page_offset,
+                       copy_size);
+            }
+
+            MAP(virtual, physical_memory_entry, PTR(current_page_vaddr));
         }
     end:
         programHeader = (void *)programHeader + header->programHeaderEntrySize;
@@ -150,7 +171,9 @@ ProcessThread *processLoadELF(Process *process, void *file_data) {
     thread->function = 0;
     initialize_thread_files(thread);
 
-    build_starting_stack(thread, PTR(header->entryPosition), phdr_virtual, header->programHeaderEntryCount, header->programHeaderEntrySize);
+    build_starting_stack(thread, PTR(header->entryPosition), phdr_virtual,
+                         header->programHeaderEntryCount,
+                         header->programHeaderEntrySize);
 
     foreach (
         process->virtual_memory_entries, VirtualMemoryEntry *,

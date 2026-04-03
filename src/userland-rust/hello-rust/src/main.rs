711819e9acd653c0b55efca32859a75664c854e9
biosfood <leisenhau@gmail.com>
2026-04-03 21:58:57 +0200

    add more complexity to the hello-rust example program

--- src/userland-rust/hello-rust/src/main.rs
+++ src/userland-rust/hello-rust/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello from the full Rust std on honey-os-2!");
+
+    let mut vec = Vec::new();
+    vec.push(1);
+    vec.push(2);
+    println!("Look, dynamic allocation works: {:?}", vec);
 }
